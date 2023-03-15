9bac1baab977c12125bb728424802bf01a0b3af6
Marvin Bangert <marvin.bangert@glueckkanja-gab.com>
2023-03-15 01:50:57 +0100

    Initial

---
+++ GraphHelper.cs
@@ -0,0 +1,87 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+
+class GraphHelper
+{
+    // Settings object
+    private static Settings? _settings;
+    // App-only auth token credential
+    private static ClientSecretCredential? _clientSecredCredential;
+    // Client configured with app-only authentication
+    private static GraphServiceClient? _appClient;
+
+    public static void InitializeGraphForAppOnlyAuth(Settings settings)
+    {
+        // Ensure settings isn't null
+        _ = settings ??
+            throw new System.NullReferenceException("Settings cannot be null");
+
+        _settings = settings;
+
+        if (_clientSecredCredential == null)
+        {
+            _clientSecredCredential = new ClientSecretCredential(
+                _settings.TenantId, _settings.ClientId, _settings.ClientSecret);
+        }
+
+        if (_appClient == null)
+        {
+            _appClient = new GraphServiceClient(_clientSecredCredential,
+                // Use the default scope, which will request the scopes
+                // configured on the app registration
+                new[] { "https://graph.microsoft.com/.default" });
+        }
+    }
+
+    public static async Task<string> GetAppOnlyTokenAsync()
+    {
+        // Ensure credentia isn't null
+        _ = _clientSecredCredential ??
+            throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+        // Request token with given scopes
+        var context = new TokenRequestContext(new[] { "https://graph.microsoft.com/.default" });
+        var response = await _clientSecredCredential.GetTokenAsync(context);
+        return response.Token;
+    }
+
+    public static Task<Microsoft.Graph.Models.SiteCollectionResponse?> GetSitesAsync()
+    {
+        // Ensure client isn't null
+        _ = _appClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+        return _appClient
+            .Sites
+            .GetAsync(requestConfiguration => {
+                requestConfiguration.QueryParameters.Select = new string[] { "Id","WebUrl" };
+                requestConfiguration.QueryParameters.Top = 5; });
+    }
+
+    //public static Task GetSiteActivitiesAsync(string siteId, string today, string earlier)
+    //{
+    //    // Ensure client isn't null
+    //    _ = _appClient ??
+    //        throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+    //    var response = _appClient.Sites[siteId]
+    //        .GetActivitiesByIntervalWithStartDateTimeWithEndDateTimeWithInterval(earlier, today, "month")
+    //        .GetAsync();
+
+
+    //    //int c = 0;
+
+    //    //var result = response
+    //    //    .Result
+    //    //    .Value;
+
+    //    //foreach( var item in result )
+    //    //{
+    //    //    Console.WriteLine(item.Access.ActionCount.ToString());
+    //    //    c = c + item.Access.ActionCount ?? 0;
+    //    //}
+
+    //    //Console.WriteLine(c);
+    //}
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,106 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Models.ODataErrors;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections;
+
+Console.WriteLine("Microsoft Graph & .NET Hackathon 2023\n");
+
+var settings = Settings.LoadSettings();
+
+// Initialize Graph
+InitializeGraph(settings);
+
+int choice = -1;
+
+while (choice != 0)
+{
+    Console.WriteLine("Please choose one of the following options:");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("1. Display access token");
+    Console.WriteLine("2. Get all SharePoint sites");
+
+    try
+    {
+        choice = int.Parse(Console.ReadLine() ?? string.Empty);
+    }
+    catch (System.FormatException )
+    {
+        // Set to invalid value
+        choice = -1;
+    }
+
+    switch(choice) 
+    {
+        case 0:
+            // Exit the program
+            Console.WriteLine("Goodby...");
+            break;
+        case 1:
+            // Display access token
+            await DisplayAccessTokenAsync();
+            break;
+        case 2:
+            // Get all SharePoint Sites
+            await GetSharePointSites();
+            break;
+        default:
+            Console.WriteLine("Invalid choice! Please try again.");
+            break;
+    }
+}
+
+void InitializeGraph(Settings settings)
+{
+    GraphHelper.InitializeGraphForAppOnlyAuth(settings);
+}
+
+async Task DisplayAccessTokenAsync()
+{
+    try 
+    {
+        var appOnlyToken = await GraphHelper.GetAppOnlyTokenAsync();
+        Console.WriteLine($"App-only token: {appOnlyToken}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting app-only access token: {ex.Message}");
+    }
+}
+
+async Task GetSharePointSites()
+{
+    try
+    {
+        //var today = DateTime.Today.ToString("yyyy-MM-dd");
+        //var earlier = DateTime.Today.AddDays(-90).ToString("yyyy-MM-dd");
+        
+        var siteResult = await GraphHelper.GetSitesAsync();
+
+        if(siteResult != null && siteResult.Value != null)
+        {
+            Console.WriteLine("WebUrl,siteId");
+            foreach (var site in siteResult.Value)
+            {
+                if(site.Id != null)
+                {
+                    var siteId = site.Id.Split(',')[1];
+                    //Task siteActivity = GraphHelper.GetSiteActivitiesAsync(siteId, today, earlier);
+                    Console.WriteLine($"{site.WebUrl} {siteId}");
+                }
+                else
+                {
+                    Console.WriteLine("Site ID is empty"); 
+                }
+            }
+        } 
+        else 
+        { 
+            Console.WriteLine("siteResult is empty"); 
+        }
+    }
+    catch (ODataError odataError)
+    {
+        Console.WriteLine($"Error getting sites: {odataError.Error.Message}");
+        throw;
+    }
+}
